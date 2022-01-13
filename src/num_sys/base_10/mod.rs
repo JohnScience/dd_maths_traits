2871bca6ca96c7e064fdb4f130ab98a277d4c5f7
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-01-12 21:22:55 -0700

    Added ToCanonicalRepresentationBase10AsString trait

--- src/num_sys/base_10/mod.rs
+++ src/num_sys/base_10/mod.rs
@@ -1,1 +1,29 @@
 pub mod int;
+
+#[cfg(any(doc, test, doctest, feature = "std"))]
+use std::string::ToString;
+#[cfg(any(doc, test, doctest, feature = "std"))]
+use num_traits::PrimInt;
+
+#[cfg(any(doc, test, doctest, feature = "std"))]
+pub trait ToCanonicalRepresentationBase10AsString {
+    fn to_canonical_representation_base_10_as_string(&self) -> String;
+}
+
+#[cfg(any(doc, test, doctest, feature = "std"))]
+impl<T> ToCanonicalRepresentationBase10AsString for T
+where T: PrimInt + ToString {
+    fn to_canonical_representation_base_10_as_string(&self) -> String {
+        self.to_string()
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use crate::num_sys::base_10::ToCanonicalRepresentationBase10AsString;
+
+    #[test]
+    pub fn to_canonical_representation_base_10_as_string_works_for_negative_primitive_signed_integers() {
+        assert_eq!((-17).to_canonical_representation_base_10_as_string(), "-17");
+    }
+}
