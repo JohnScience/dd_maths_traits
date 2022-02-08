3224e9fbb612df1a9f7afc8419cf784e8e7743b0
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-02-08 12:01:17 -0700

    Made the code more readable

--- src/num_sys/base_10/int/sum_digits.rs
+++ src/num_sys/base_10/int/sum_digits.rs
@@ -20,7 +20,7 @@ pub trait SumDigits {
             Number = Self,
             Base = Base,
             Accumulator = Accumulator,
-            Modulo = Modulo,
+            OptModulo = Modulo,
             Output = Output,
         >,
     {
@@ -39,7 +39,7 @@ type DefaultBase = u128;
     not(feature = "gen_assoc_consts"),
     any(doc, test, doctest, feature = "adt_const_params")
 ))]
-type DefaultModulo = Option<u128>;
+type DefaultOptModulo = Option<u128>;
 
 #[cfg(all(
     not(feature = "gen_assoc_consts"),
@@ -49,26 +49,27 @@ pub trait SumDigits {
     fn sum_digits<
         Base,
         Accumulator,
-        Modulo,
+        OptModulo,
         Output,
         const BASE: DefaultBase,
-        const MODULO: DefaultModulo,
+        const OPT_MODULO: DefaultOptModulo,
     >(
         &self,
     ) -> Output
     where
         Self: Sized,
-        (Self, Base, Accumulator, Modulo, Output): SumDigitsTypeTuple<
+        (Self, Base, Accumulator, OptModulo, Output): SumDigitsTypeTuple<
             Number = Self,
             Base = Base,
             Accumulator = Accumulator,
-            Modulo = Modulo,
+            OptModulo = OptModulo,
             Output = Output,
         >,
     {
-        <(Self, Base, Accumulator, Modulo, Output) as SumDigitsTypeTuple>::sum_digits::<BASE, MODULO>(
-            self,
-        )
+        <(Self, Base, Accumulator, OptModulo, Output) as SumDigitsTypeTuple>::sum_digits::<
+            BASE,
+            OPT_MODULO,
+        >(self)
     }
 }
 
@@ -76,11 +77,7 @@ pub trait SumDigits {
     not(feature = "gen_assoc_consts"),
     any(doc, test, doctest, feature = "adt_const_params")
 ))]
-impl<T> SumDigits for T
-where
-    T: SumDigitsDefaults
-{
-}
+impl<T> SumDigits for T where T: SumDigitsDefaults {}
 
 #[cfg(all(
     not(feature = "gen_assoc_consts"),
@@ -90,9 +87,9 @@ pub trait SumDigitsTypeTuple {
     type Number;
     type Base;
     type Accumulator;
-    type Modulo;
+    type OptModulo;
     type Output;
-    fn sum_digits<const BASE: DefaultBase, const MODULO: DefaultModulo>(
+    fn sum_digits<const BASE: DefaultBase, const OPT_MODULO: DefaultOptModulo>(
         n_ref: &Self::Number,
     ) -> Self::Output;
 }
@@ -101,21 +98,25 @@ pub trait SumDigitsTypeTuple {
     not(feature = "gen_assoc_consts"),
     any(doc, test, doctest, feature = "adt_const_params")
 ))]
-impl<Number, Base, Accumulator, Modulo, Output> SumDigitsTypeTuple
-    for (Number, Base, Accumulator, Modulo, Output)
+impl<Number, Base, Accumulator, OptModulo, Output> SumDigitsTypeTuple
+    for (Number, Base, Accumulator, OptModulo, Output)
 where
-    Number:
-        SumDigitsDefaults<Base = Base, Accumulator = Accumulator, Modulo = Modulo, Output = Output>,
+    Number: SumDigitsDefaults<
+        Base = Base,
+        Accumulator = Accumulator,
+        OptModulo = OptModulo,
+        Output = Output,
+    >,
 {
     type Number = Number;
     type Base = Base;
     type Accumulator = Accumulator;
-    type Modulo = Modulo;
+    type OptModulo = OptModulo;
     type Output = Output;
-    fn sum_digits<const BASE: DefaultBase, const MODULO: DefaultModulo>(
+    fn sum_digits<const BASE: DefaultBase, const OPT_MODULO: DefaultOptModulo>(
         n_ref: &Self::Number,
     ) -> Self::Output {
-        n_ref.generic_default_sum_digits::<BASE, MODULO>()
+        n_ref.generic_std_sum_digits::<BASE, OPT_MODULO>()
     }
 }
 
@@ -126,13 +127,13 @@ where
 pub trait SumDigitsDefaults {
     type Base;
     type Accumulator;
-    type Modulo;
+    type OptModulo;
     type Output;
     const BASE: DefaultBase;
-    const MODULO: DefaultModulo;
+    const NO_MODULO: DefaultOptModulo;
 
-    fn conc_default_sum_digits(&self) -> Self::Output;
-    fn generic_default_sum_digits<const BASE: DefaultBase, const MODULO: DefaultModulo>(
+    fn std_sum_digits(&self) -> Self::Output;
+    fn generic_std_sum_digits<const BASE: DefaultBase, const OPT_MODULO: DefaultOptModulo>(
         &self,
     ) -> Self::Output;
 }
@@ -147,15 +148,15 @@ pub trait SumDigitsDefaults {
 impl SumDigitsDefaults for u8 {
     type Base = u8;
     type Accumulator = u8;
-    type Modulo = u8;
+    type OptModulo = u8;
     // 199 is the number with the highest sum of digits in Accumulator.
     // Output must be able to hold values from 0 to 1+9+9=19
     type Output = u8;
 
     const BASE: DefaultBase = 10;
-    const MODULO: DefaultModulo = None;
+    const NO_MODULO: DefaultOptModulo = None;
 
-    fn generic_default_sum_digits<const BASE: DefaultBase, const MODULO: DefaultModulo>(
+    fn generic_std_sum_digits<const BASE: DefaultBase, const OPT_MODULO: DefaultOptModulo>(
         &self,
     ) -> Self::Output {
         let base: u8 = if BASE > 255 { 255 } else { BASE as u8 };
@@ -169,7 +170,7 @@ impl SumDigitsDefaults for u8 {
                 break;
             };
         }
-        match MODULO {
+        match OPT_MODULO {
             None => acc,
             Some(modulo) => {
                 if modulo >= 255 {
@@ -182,8 +183,8 @@ impl SumDigitsDefaults for u8 {
     }
 
     #[inline(always)]
-    fn conc_default_sum_digits(&self) -> Self::Output {
-        self.generic_default_sum_digits::<{ Self::BASE }, { Self::MODULO }>()
+    fn std_sum_digits(&self) -> Self::Output {
+        self.generic_std_sum_digits::<{ Self::BASE }, { Self::NO_MODULO }>()
     }
 }
 
@@ -191,42 +192,41 @@ impl SumDigitsDefaults for u8 {
 mod tests {
     #[test]
     fn sum_digits_works_for_default_type_tuple_of_u8() {
-        use crate::num_sys::base_10::int::sum_digits::{SumDigitsTypeTuple, DefaultBase, DefaultModulo};
+        use crate::num_sys::base_10::int::sum_digits::{
+            DefaultBase, DefaultOptModulo, SumDigitsTypeTuple,
+        };
 
-        const BASE: DefaultBase = 10;
-        const MODULO: DefaultModulo = None;
+        const BASE_10: DefaultBase = 10;
+        const NO_MODULO: DefaultOptModulo = None;
         assert_eq!(
-            <(u8,u8,u8,u8,u8) as SumDigitsTypeTuple>::sum_digits::<BASE, MODULO>(&5u8), 5u8
+            <(u8, u8, u8, u8, u8) as SumDigitsTypeTuple>::sum_digits::<BASE_10, NO_MODULO>(&5u8),
+            5u8
         );
     }
 
     #[test]
     fn sum_digits_for_5u8() {
-        use crate::num_sys::base_10::int::sum_digits::{SumDigits, DefaultBase, DefaultModulo};
+        use crate::num_sys::base_10::int::sum_digits::SumDigitsDefaults as D;
 
-        type Base = u8;
-        type Modulo = u8;
-        type Accumulator = u8;
-        type Output = u8;
-        const BASE: DefaultBase = 10;
-        const MODULO: DefaultModulo = None;
-        assert_eq!(
-            5u8.sum_digits::<Base,Modulo,Accumulator,Output,BASE, MODULO>(), 5u8
-        )
+        assert_eq!(5u8.std_sum_digits(), 5u8)
     }
 
     #[test]
     fn sum_digits_mod_2_works_for_5u8() {
-        use crate::num_sys::base_10::int::sum_digits::{SumDigits, DefaultBase, DefaultModulo};
+        use crate::num_sys::base_10::int::sum_digits::{
+            DefaultBase, DefaultOptModulo, SumDigits, SumDigitsDefaults as D,
+        };
 
-        type Base = u8;
-        type Modulo = u8;
-        type Accumulator = u8;
-        type Output = u8;
-        const BASE: DefaultBase = 10;
-        const MODULO: DefaultModulo = Some(2);
+        // https://internals.rust-lang.org/t/named-type-parameters/6921/20
+        type Base = <u8 as D>::Base;
+        type OptModulo = <u8 as D>::OptModulo;
+        type Accumulator = <u8 as D>::Accumulator;
+        type Output = <u8 as D>::Output;
+        const BASE_10: DefaultBase = <u8 as D>::BASE;
+        const MODULO_2: DefaultOptModulo = Some(2);
         assert_eq!(
-            5u8.sum_digits::<Base,Modulo,Accumulator,Output,BASE, MODULO>(), 1u8
+            5u8.sum_digits::<Base, OptModulo, Accumulator, Output, BASE_10, MODULO_2>(),
+            1u8
         )
     }
 }
